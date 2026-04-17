05af66848d9894c326384225cfc1d2fbd1e5c3ae
Pragyat Nikunj <pragyatnikunj@gmail.com>
2026-04-17 23:15:45 +0530

    feat: Added margin Ruler Componenent

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -12,6 +12,7 @@ import Highlight from '@tiptap/extension-highlight';
 
 import { FontSizeExtension } from '@/extensions/font-size';
 import { LineHeightExtension } from '@/extensions/line-height';
+import { Ruler } from './ruler';
 
 export const Editor = () => {
     const { setEditor } = useEditorStore();
@@ -94,6 +95,7 @@ export const Editor = () => {
     });
     return (
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <Ruler />
             <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
                 <EditorContent editor={editor} />
             </div>
